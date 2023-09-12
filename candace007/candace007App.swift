7bd737de064749f5a0d69a1348457cd3768fa59c
taissa-rodrigues <141452801+taissa-rodrigues@users.noreply.github.com>
2023-09-12 13:47:19 -0300

    Initial Commit

---
+++ candace007/candace007App.swift
@@ -0,0 +1,17 @@
+//
+//  candace007App.swift
+//  candace007
+//
+//  Created by User on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct candace007App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
